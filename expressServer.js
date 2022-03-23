31d2858b5e23aebec7a96d693d5e4aff8b5f4ba7
Alvin <alvin.tol@hotmail.com>
2022-03-23 04:29:03 +0000

    clean up registration page and create user object to populate new Users moving forward

--- expressServer.js
+++ expressServer.js
@@ -18,6 +18,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 // const validCookie = (req, res, next) => {
 //   const { cookies } = req;
 //   if (cookies) {
@@ -43,6 +56,10 @@ app.post('/login', (req, res) => {
   }
   res.cookie('username', req.body.username);
   res.redirect('/urls');
+});
+
+app.post('/register', (req, res) => {
+  res.render('register');
 });
 
 app.post('/logout', (req, res) => {
